9e8cc48349b19280a4fd148f533c80566aa93046
codename-123 <wltkd5139@naver.com>
2026-03-21 07:28:59 +0900

    Update

---
+++ _posts/lfi.py
@@ -0,0 +1,6 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('0,0,0,0', 2222))
+server_socket.listen(5)
