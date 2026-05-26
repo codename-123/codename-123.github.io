5ee8cecf4071ca20e8fc367a07fbe38cdf5ccda1
codename-123 <wltkd5139@naver.com>
2026-05-27 08:23:58 +0900

    Update

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -23,6 +23,20 @@ var idx = lunr(function () {
 });
 
 $(document).ready(function() {
+  function truncateExcerpt(excerpt, limit) {
+    if (!excerpt) {
+      return "";
+    }
+
+    var words = excerpt.split(" ");
+
+    if (words.length <= limit) {
+      return excerpt;
+    }
+
+    return words.splice(0, limit).join(" ") + "...";
+  }
+
   $('input#search').on('keyup', function () {
     var resultdiv = $('#results');
     var query = $(this).val().toLowerCase();
@@ -31,17 +45,21 @@ $(document).ready(function() {
         query.split(lunr.tokenizer.separator).forEach(function (term) {
           q.term(term, { boost: 100 })
           if(query.lastIndexOf(" ") != query.length-1){
-            q.term(term, {  usePipeline: false, wildcard: lunr.Query.wildcard.TRAILING, boost: 10 })
+            q.term(term, { usePipeline: false, wildcard: lunr.Query.wildcard.TRAILING, boost: 10 })
           }
           if (term != ""){
-            q.term(term, {  usePipeline: false, editDistance: 1, boost: 1 })
+            q.term(term, { usePipeline: false, editDistance: 1, boost: 1 })
           }
         })
       });
+
     resultdiv.empty();
     resultdiv.prepend('<p class="results__found">'+result.length+' {{ site.data.ui-text[site.locale].results_found | default: "Result(s) found" }}</p>');
+
     for (var item in result) {
       var ref = result[item].ref;
+      var excerpt = truncateExcerpt(store[ref].excerpt, 100);
+
       if(store[ref].teaser){
         var searchitem =
           '<div class="list__item">'+
@@ -52,21 +70,22 @@ $(document).ready(function() {
               '<div class="archive__item-teaser">'+
                 '<img src="'+store[ref].teaser+'" alt="">'+
               '</div>'+
-              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+excerpt+'</p>'+
             '</article>'+
           '</div>';
       }
       else{
-    	  var searchitem =
+        var searchitem =
           '<div class="list__item">'+
             '<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">'+
               '<h2 class="archive__item-title" itemprop="headline">'+
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
               '</h2>'+
-              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+excerpt+'</p>'+
             '</article>'+
           '</div>';
       }
+
       resultdiv.append(searchitem);
     }
   });
